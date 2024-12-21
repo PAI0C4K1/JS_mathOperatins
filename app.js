597daeff281f5049109a6525a494de2769061bcb
Baiel <brevno776@gmail.com>
2024-12-21 13:29:00 +0600

    Another way

---
+++ app.js
@@ -0,0 +1,6 @@
+let zanchenie_A = 10
+let zanchenie_B = 2
+console.log ( "Сложение: " +  zanchenie_A + " + " + zanchenie_B + " = " + (zanchenie_A + zanchenie_B))
+console.log ( "Вычитание: " +  zanchenie_A + " - " + zanchenie_B + " = " + (zanchenie_A - zanchenie_B))
+console.log ( "Умножение: " +  zanchenie_A + " * " + zanchenie_B + " = " + (zanchenie_A * zanchenie_B))
+console.log ( "Деление: " +  zanchenie_A + " / " + zanchenie_B + " = " + (zanchenie_A / zanchenie_B))
